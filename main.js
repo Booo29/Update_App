5bdb20f331d604ace5079f7ba50d4aa1177b3c22
Booo29 <joshua.granados.loria@gmail.com>
2025-01-14 17:58:57 -0600

    update

--- main.js
+++ main.js
@@ -4,50 +4,50 @@ const { autoUpdater } = require('electron-updater');
 let mainWindow;
 
 function createWindow () {
-    mainWindow = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        nodeIntegration: true,
-      },
-    });
-    mainWindow.loadFile('index.html');
-    mainWindow.on('closed', function () {
-      mainWindow = null;
-    });
-  }
-  
-  app.on('ready', () => {
-    createWindow();
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
   });
-  
-  app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
+  mainWindow.loadFile('index.html');
+  mainWindow.on('closed', function () {
+    mainWindow = null;
   });
-  
-  app.on('activate', function () {
-    if (mainWindow === null) {
-      createWindow();
-    }
-  });
-  
-  ipcMain.on('app_version', (event) => {
-    event.sender.send('app_version', { version: app.getVersion() });
-  });
-
   mainWindow.once('ready-to-show', () => {
     autoUpdater.checkForUpdatesAndNotify();
   });
+}
 
-  autoUpdater.on('update-available', () => {
-    mainWindow.webContents.send('update_available');
-  });
-  autoUpdater.on('update-downloaded', () => {
-    mainWindow.webContents.send('update_downloaded');
-  });
+app.on('ready', () => {
+  createWindow();
+});
 
-  ipcMain.on('restart_app', () => {
-    autoUpdater.quitAndInstall();
-  });
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
+});
+
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+});
